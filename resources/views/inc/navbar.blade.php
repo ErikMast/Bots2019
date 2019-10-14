9fe00f60043da8326a473d97160ec06276e9544a
Rien <helloitsrien@gmail.com>
2019-10-14 18:51:31 +0200

    Changed some things

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -15,7 +15,7 @@
                   </ul>
 
                   <!-- Right Side Of Navbar -->
-                  <ul class="navbar-nav ml-auto">
+                  <ul class="navbar-nav ml-auto test">
                       <!-- Authentication Links -->
                       @guest
                           <li class="nav-item">
