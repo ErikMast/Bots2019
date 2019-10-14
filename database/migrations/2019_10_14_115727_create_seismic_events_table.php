cfb5aaa590f70698861d6e675bb46ad65ddc86c2
Rien <helloitsrien@gmail.com>
2019-10-14 15:16:33 +0200

    Added seismic event migration and model

---
+++ database/migrations/2019_10_14_115727_create_seismic_events_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSeismicEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seismic_events', function (Blueprint $table) {
+
+            $table->bigIncrements('id');
+
+            $table->double("long");
+            $table->double("lat");
+
+            $table->integer("mag");
+            $table->string("type");
+            $table->date("date");
+            $table->string("place");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seismic_events');
+    }
+}
