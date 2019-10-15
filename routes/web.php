c6df3e9d8dfc9e123c534c5cb35824b791c8bd94
Rien <helloitsrien@gmail.com>
2019-10-15 11:17:22 +0200

    Route fix

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'DashboardController@index')->name('dashboard.index');
+Route::get('/report', 'ReportController@index')->name('report.index');
+Route::post('/report', 'ReportController@store')->name('report.store')->middleware('throttle:10,1');
+
+Auth::routes(["register" => false]);
+
+Route::prefix("api/v1")->name('api.')->group(function() {
+    Route::get('/seismic/line', 'SeismicController@lineChart')->name('seismic.line');
+    Route::get('/seismic/heatmap', 'SeismicController@heatMapChart')->name('seismic.heat');
+    Route::get('/seismic/seperator', 'SeismicController@typeSeperatorGraph')->name('seismic.seperator');
+}
+);
+
+Route::middleware("auth")->group(function() {
+    Route::get('/accounts', 'PagesController@accounts')->name('pages.accounts');
+    Route::get('/reports', 'ReportController@reports')->name('report.reports');
+});
